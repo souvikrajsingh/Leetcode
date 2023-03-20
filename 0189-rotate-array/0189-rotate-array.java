baed564e43599d4aa3107509883f4ff5cc830e77
Souvik Raj Singh <81793119+souvikrajsingh@users.noreply.github.com>
2023-03-20 13:20:03 +0530

    Time: 1 ms (58.45%), Space: 58.8 MB (22.23%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,16 +1,25 @@
 class Solution {
     public void rotate(int[] nums, int k) {
-        int[] temp = new int[nums.length];
-        
-//         here we are using the modulo property
-        
-        for(int  i = 0; i< nums.length; i ++){
-            temp[(i+k) % nums.length] = nums[i];
-            
-        }
-        
-        for(int i = 0 ; i<nums.length; i++){
-            nums[i]=temp[i];
-        }
+    if(nums == null || nums.length < 2){
+        return;
     }
+    
+    k = k % nums.length;
+    reverse(nums, 0, nums.length - k - 1);
+    reverse(nums, nums.length - k, nums.length - 1);
+    reverse(nums, 0, nums.length - 1);
+    
+}
+
+private void reverse(int[] nums, int i, int j){
+    int tmp = 0;       
+    while(i < j){
+        tmp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tmp;
+        i++;
+        j--;
+    }
+    }
+
 }
