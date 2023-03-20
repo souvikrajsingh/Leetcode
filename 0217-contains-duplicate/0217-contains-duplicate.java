5e7ca17d22f3ba69da337aa6943e4fa735914d2b
Souvik Raj Singh <81793119+souvikrajsingh@users.noreply.github.com>
2023-03-20 22:24:19 +0530

    Time: 11 ms (75.57%), Space: 56.3 MB (45.47%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+    HashSet<Integer> hset = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (hset.contains(nums[i])) {
+                return true;
+            } else {
+                hset.add(nums[i]);
+            }
+        }
+        return false;
+}
+}
