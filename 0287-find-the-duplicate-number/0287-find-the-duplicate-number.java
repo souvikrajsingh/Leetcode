ca7b51af707f30a4236cc6152b5a22a34a3b8a54
Souvik Raj Singh <81793119+souvikrajsingh@users.noreply.github.com>
2023-04-18 11:23:06 +0530

    Time: 23 ms (35.08%), Space: 54.9 MB (98.26%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -1,7 +1,24 @@
 class Solution {
     public int findDuplicate(int[] nums) {
         
-        int i= 0;
+        
+/* Hashmaps*/
+        HashMap<Integer,Integer> numMap = new HashMap<>();
+        for(int i = 0; i < nums.length;i++){
+            if(numMap.containsKey(nums[i])){
+                return nums[i];
+            }
+            numMap.put(nums[i] , i);
+        }
+        
+        return -1;
+    }
+}
+        
+        
+
+/* Cyclic sort method */
+       /* int i= 0;
         while( i < nums.length){
             if(nums[i] != i + 1){
                 int correct  = nums[i]  -1;
@@ -19,5 +36,4 @@ class Solution {
             }
         }
         return -1;
-    }
-}
+    }*/
