8a9040d71addfb31620bd91c69d83a08ff1f09af
Souvik Raj Singh <81793119+souvikrajsingh@users.noreply.github.com>
2023-04-20 03:39:33 +0530

    Time: 1 ms (97.71%), Space: 42.9 MB (51.05%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,6 +1,44 @@
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> ansList= new ArrayList<>();
+        recursePermute(0 , nums, ansList);
+        return ansList;
+    }
+    public void recursePermute(int ind , int[] nums, List<List<Integer>> ansList){
+        if(ind == nums.length){
+            List<Integer> list = new ArrayList<>();
+            for(int i = 0 ; i< nums.length; i++){
+                list.add(nums[i]);
+            }
+            ansList.add(new ArrayList<>(list));
+            return;
+        }
+        
+        for(int i = ind; i< nums.length; i++){
+            swap(ind , i , nums);
+            recursePermute(ind + 1, nums, ansList);
+            swap(ind , i , nums);
+        }
+        
+        
+        }
+    
+    public void swap(int i , int j , int[] nums){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+    }
+    
+
+/*
+
+This is Permutation 1 taking time complexity of O(n!)+O(n);
+Space complexity of: O(n)[for ds we taking] + O(n)[for the mapping array freq]
+
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> ansList= new ArrayList<>();
         List<Integer> ds = new ArrayList<>();
         boolean[] freq= new boolean[nums.length];
         recursePermute(nums, ds, ansList, freq);
@@ -9,7 +47,7 @@ class Solution {
     public void recursePermute(int[]nums, List<Integer> ds, List<List<Integer>> ansList, boolean[] freq){
         
         /*if the data structure is the size of the array it self, then supposedly it contains one of the permutations.*/
-        
+         /*
         if(ds.size() == nums.length){
             ansList.add(new ArrayList<>(ds));
             return;
@@ -25,3 +63,5 @@ class Solution {
     }
     }
 }
+
+*/
