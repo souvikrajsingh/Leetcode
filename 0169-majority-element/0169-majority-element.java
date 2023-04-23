103eb41e610bdc1623138c020d8489e9ea6c7be6
Souvik Raj Singh <81793119+souvikrajsingh@users.noreply.github.com>
2023-04-23 23:05:27 +0530

    Time: 1 ms (99.47%), Space: 46.5 MB (42.77%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,5 +1,38 @@
 class Solution {
    public  int majorityElement(int[] nums) {
+       
+//        moore's voting algorithm - optimal approach
+
+        int cnt =0 ;
+        int el = 0;
+
+        for(int i  = 0; i< nums.length; i++){
+            if(cnt == 0){
+                el = nums[i];
+                cnt = 1;
+            }
+            else if(el == nums[i]){
+                cnt++;
+            }
+            else{
+                cnt--;
+            }
+        }
+        int finalCnt = 0;
+        for (int i = 0; i < nums.length ; i++) {
+            if(nums[i] == el){
+                finalCnt++;
+            }
+        }
+
+        if(finalCnt > (nums.length /2)){
+            return el;
+        }
+
+        return -1;
+       
+       
+       /* better approach
         HashMap<Integer , Integer> hMap = new HashMap<>();
 
         for (int i = 0; i < nums.length; i++) {
@@ -14,5 +47,6 @@ class Solution {
         }
 
         return  -1;
+        */
 }
 }
