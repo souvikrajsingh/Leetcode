ea284485b1d9a72cdb55ba3a79f2b097ad0b011d
Souvik Raj Singh <81793119+souvikrajsingh@users.noreply.github.com>
2023-05-09 14:21:02 +0530

    Time: 2 ms (96.57%), Space: 46.3 MB (51.85%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,19 +1,52 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        ArrayList<Integer> ansList = new ArrayList<>();
-     HashMap<Integer , Integer> hMap = new HashMap<>();
-
-        for (int i = 0; i < nums.length; i++) {
-            int value = hMap.getOrDefault(nums[i], 0);
-            hMap.put(nums[i], value + 1);
-        }
-
-        for(Map.Entry<Integer ,Integer> e : hMap.entrySet()){
-            if(e.getValue()  > nums.length / 3 ){
-             ansList.add(e.getKey());
+        int num1 = -1;
+        int num2 = -1;
+        
+        int c1 = 0;
+        int c2 = 0;
+        
+        int len = nums.length;
+        
+        for( int i =0; i< len;i++) {
+            if(nums[i] == num1){
+                c1++;
+            }
+            else if(nums[i] == num2){
+                c2++;
+            }
+            else if( c1 == 0){
+                num1 = nums[i];
+                c1 = 1;
+            }
+            else if(c2 == 0){
+                num2 = nums[i];
+                c2= 1;
+            }
+            else{
+                c1--;
+                c2--;
             }
         }
-
-        return ansList;
-}
+        
+        List<Integer> ans = new ArrayList<Integer>();
+        c1 = 0;
+        c2 = 0;
+        for(int i = 0; i< len; i ++){
+            if(nums[i] == num1){
+                c1++;
+            }
+            else if(nums[i] == num2){
+                c2++;
+            }
+        }
+        
+        if(c1  > len/3){
+            ans.add(num1);
+        }
+        if(c2 > len/3){
+            ans.add(num2);
+        }
+        return ans;
+    }
 }
