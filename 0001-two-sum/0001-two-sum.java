d3e98fcfcf28af031441e7649f76ca20f3577db5
Souvik Raj Singh <81793119+souvikrajsingh@users.noreply.github.com>
2023-07-02 22:06:21 +0530

    Time: 1 ms (99.34%), Space: 43.7 MB (53.21%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,27 +1,6 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        
-        
-/* brute force approach 
-Time complexity  = O(n^2)
-        int[] sum = new int[2];
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = i+1; j < nums.length; j++) {
-                if(i!=j && (nums[i] + nums[j] == target)){
-                    sum[0] = i;
-                    sum[1] = j;
-                }
-            }
-        }
-
-        return sum;*/
-        
-        
-/*optimised solution using hashmaps
-
-   Time Complexity O(n)*/
-        
-        int[] results  = new int [2];
+         int[] results  = new int [2];
         Map<Integer , Integer> map = new HashMap<Integer,Integer>();
         for(int i  = 0 ; i < nums.length;i++){
             if(map.containsKey(target - nums[i])){
@@ -35,3 +14,4 @@ Time complexity  = O(n^2)
         
     }
 }
+  
